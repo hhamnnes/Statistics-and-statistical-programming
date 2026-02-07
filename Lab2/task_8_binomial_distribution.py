744ec367dcc3648690ff92fa7b393afe5b38b9b8
hhamnnes <96078953+hhamnnes@users.noreply.github.com>
2026-02-07 22:31:45 +0100

    First written program

--- Lab2/task_8_binomial_distribution.py
+++ Lab2/task_8_binomial_distribution.py
@@ -2,5 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import binom as bi
 
+n = int(input("Tast inn antall forsøk: "))
+p = float(input("Tast inn sannsynligheten: "))
 
 
+x = np.arange(n+1)
+
+pmf = bi.pmf(x, n, p)
+cdf = bi.cdf(x, n, p)
+
+plt.figure()
+plt.plot(x, pmf, label="Punktsannsynlighet")
+plt.plot(x, cdf, label="Fordelingsfunksjonen")
+
+plt.title(f"Binomisk fordeling for verdiene n={n} og p={p}")
+plt.xlabel("Punkt P(X=x)")
+plt.ylabel("Sannsynlighet")
+plt.legend()
+plt.grid(True)
+
+plt.show()
