c516cfd3527d311bc92b90629b4052742ff7bda6
hhamnnes <96078953+hhamnnes@users.noreply.github.com>
2026-02-22 13:45:18 +0100

    I have compleated task 10 b)

---
+++ Lab3/task_10_b.py
@@ -0,0 +1,16 @@
+from scipy.stats import norm
+import numpy as np
+import matplotlib.pyplot as plt
+
+def likelihood_for_higher_weight(weight):
+    mu = 760
+    sigma = 10
+    likelihood = 1 - norm.cdf(weight, mu, sigma)
+    return likelihood
+
+# Task b)
+weight = float(input("Enter a weight in grams: "))
+
+likelihood = likelihood_for_higher_weight(weight)
+print(f"The likelihood of a weight being higher than {weight} grams is {likelihood:.4f}")
+print(f"Or {likelihood * 100:.2f}%")
