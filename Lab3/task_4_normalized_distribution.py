80ccd58e4a11af4d5e7a082187deeb6d58ad9683
hhamnnes <96078953+hhamnnes@users.noreply.github.com>
2026-02-22 13:32:25 +0100

    Compleated task 10 a)

--- Lab3/task_4_normalized_distribution.py
+++ Lab3/task_4_normalized_distribution.py
@@ -1,4 +1,16 @@
 from scipy.stats import norm
+import numpy as np
 import matplotlib.pyplot as plt
 
-def normalized_distribution(mu, sigma, label):
+def normalized_distribution(mu, sigma, start, stop, label):
+    x = np.linspace(start, stop, 1000)
+    y = norm.pdf(x, mu, sigma)
+    plt.plot(x, y, label=label)
+    plt.title("Normalfordeling")
+    plt.xlabel("x")
+    plt.ylabel("f(x)")
+    plt.show()
+
+# Task a)
+normalized_distribution(760, 10, 720, 800, "a) μ=760, σ=10")
+
